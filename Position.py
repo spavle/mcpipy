0d36c6f72114abc4515955593eeeb26c729b0d4d
spavle <pavle.spoljaric@gmail.com>
2016-02-14 19:07:11 +0100

    filename change

--- Position.py
+++ Position.py
@@ -1,4 +1,5 @@
 """
+Class for storing and manipulating coordintes in Minecraft 
 -x
 -y
 -z
@@ -12,3 +13,34 @@ rotate_right
 real_coords
 origin
 """
+
+
+class Position ( object ):
+
+   def __init__ (self, input=None , dX=None, dY=None, dZ=None ) :
+
+      if input != None :
+         self.x = input.x
+         self.y = input.y
+         self.z = input.z
+
+      if dX :
+         self.x += dX
+      if dY :
+         self.y += dY
+      if dZ :
+         self.z += dZ
+   
+
+
+
+if __name__ == "__main__":    #direct call for testing purpose
+   # self test code
+   first = Position ()
+   first.x = 2
+   first.y = 3
+   first.z = 4
+   second = Position ( first )
+   print second.x
+   third = Position ( second , 2 , 3 , 4 )
+   print third.x
