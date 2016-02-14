9350ea7426902e89181b17e8ea900e5256914584
spavle <pavle.spoljaric@gmail.com>
2016-02-14 20:00:18 +0100

    position class development

--- Position.py
+++ Position.py
@@ -1,3 +1,5 @@
+from mc import * #Minecraft api import
+mc = Minecraft() #initialization
 """
 Class for storing and manipulating coordintes in Minecraft 
 -x
@@ -17,12 +19,17 @@ origin
 
 class Position ( object ):
 
+
    def __init__ (self, input=None , dX=None, dY=None, dZ=None ) :
 
       if input != None :
          self.x = input.x
          self.y = input.y
          self.z = input.z
+         self.x_direction = input.x_direction
+         self.z_direction = input.z_direction
+      else:
+         self.get_origin ()
 
       if dX :
          self.x += dX
@@ -31,12 +38,28 @@ class Position ( object ):
       if dZ :
          self.z += dZ
    
-
+   
+   def get_origin ( self ):
+   
+      current_position = mc.player.getPos()	#coordinates
+      self.x = current_position.x
+      self.y = current_position.y
+      self.z = current_position.z
+      
+      current_direction = mc.player.getDirection () #directions
+      self.x_direction = 0
+      self.z_direction = 0
+      if abs (current_direction.x) > abs (current_direction.z): 		#nadje se dominanti smjer i spremi u vektor
+         self.x_direction=round(current_direction.x)
+      else:
+         self.z_direction=round(current_direction.z)  
+   
 
 
 if __name__ == "__main__":    #direct call for testing purpose
    # self test code
    first = Position ()
+   print first.x
    first.x = 2
    first.y = 3
    first.z = 4
