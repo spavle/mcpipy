933678c63d98b031f1eafcc6e7fa94dddda1732c
spavle <spavle@users.noreply.github.com>
2016-03-30 13:19:10 +0200

    PEP 8
    reformating

--- Position.py
+++ Position.py
@@ -1,106 +1,113 @@
-from mc import * #Minecraft api import
-mc = Minecraft() #initialization
-"""
-Class for storing and manipulating coordintes in Minecraft 
-- x
-- y
-- z
-- x_direction
-- z_direction
+from mc import *  # Minecraft api import
 
-__init__
-move ?
-copy ?
-rotate_left
-rotate_right
-origin
-"""
+mc = Minecraft()  # initialization
 
 
-class Position ( object ):
-    #clas for holding object position and direction
-    def __init__ (self, input=None , dX=0, dY=0, dZ=0 ) :
-        #class Position initialization
-        if input != None :
-            self.x = input.x
-            self.y = input.y
-            self.z = input.z
-            self.x_direction = input.x_direction
-            self.z_direction = input.z_direction
+class Position(object):
+    """
+    class for holding object position and direction
+    """
+
+    def __init__(self, my_input=None, dx=0, dy=0, dz=0):
+        # type: (Position, float, float, float) ->         Position
+        """
+        class Position initialization
+        :param my_input: Position
+        :param dx: float
+        :param dy: float
+        :param dz: float
+        :rtype: Position
+        """
+
+        if my_input is None:
+            self.get_origin()
         else:
-            self.get_origin ()
+            self.x = my_input.x
+            self.y = my_input.y
+            self.z = my_input.z
+            self.x_direction = my_input.x_direction
+            self.z_direction = my_input.z_direction
 
-        print self.x
-        print self.x_direction
-        print dX
-        self.x=self.x + self.x_direction*dX - self.z_direction*dZ    		# x translate
-        self.z=self.z + self.x_direction*dZ + self.z_direction*dX			# y translate
-        self.y = self.y + dY                                              # z translate
+        self.x = self.x + self.x_direction * dx - self.z_direction * dz  # x translate
+        self.z = self.z + self.x_direction * dz + self.z_direction * dx  # y translate
+        self.y += dy  # z translate
 
-   
-    def get_origin ( self ):
-        #get current position and direction
-        current_position = mc.player.getPos()	#get coordinates
+    def get_origin(self):
+        """
+        get current position and direction
+        """
+        current_position = mc.player.getPos()  # get coordinates
         self.x = current_position.x
         self.y = current_position.y
         self.z = current_position.z
-      
-        current_direction = mc.player.getDirection () #get direction
+        current_direction = mc.player.getDirection()  # get direction
         self.x_direction = 0
         self.z_direction = 0
-        if abs (current_direction.x) >= abs (current_direction.z): 		#find dominant direction
-            self.x_direction=int(round(current_direction.x))
+        if abs(current_direction.x) >= abs(current_direction.z):  # find dominant direction
+            self.x_direction = int(round(current_direction.x))
         else:
-            self.z_direction=int(round(current_direction.z))  
-   
-    def rotate_left (self):
-        #rotate direction to left
-        convert = {}
-        convert [ ( 1 , 0  ) ] = (  0 , -1  ) # look north
-        convert [ ( -1 , 0 ) ] = (  0 , 1 ) # look south
-        convert [ ( 0 , 1 ) ] = (  1 , 0 )  # look east
-        convert [ ( 0 , -1 ) ]= (  -1 , 0 )  # look weast
-        buff = convert [ ( self.x_direction , self.z_direction )   ]
-        self.x_direction=buff [0]
-        self.z_direction=buff [1]
-      
-    def rotate_right (self):
-        #rotate direction to right
-        convert = {}
-        convert [ ( 1 , 0  ) ] = (  0 , 1  ) # look north
-        convert [ ( -1 , 0 ) ] = (  0 , -1 ) # look south
-        convert [ ( 0 , 1 ) ] = (  -1 , 0 )  # look east
-        convert [ ( 0 , -1 ) ] = (  1 , 0 )  # look weast
-        buff = convert [ ( self.x_direction , self.z_direction )   ]
-        self.x_direction=buff [0]
-        self.z_direction=buff [1]      
-   
-   
+            self.z_direction = int(round(current_direction.z))
+
+    def rotate_left(self):
+        # rotate direction to left
+        convert = dict()
+        convert[(1, 0)] = (0, -1)  # look north
+        convert[(-1, 0)] = (0, 1)  # look south
+        convert[(0, 1)] = (1, 0)  # look east
+        convert[(0, -1)] = (-1, 0)  # look weast
+        buff = convert[(self.x_direction, self.z_direction)]
+        self.x_direction = buff[0]
+        self.z_direction = buff[1]
+
+    def rotate_right(self):
+        """
+        rotate direction to right
+        """
+        convert = dict()
+        convert[(1, 0)] = (0, 1)  # look north
+        convert[(-1, 0)] = (0, -1)  # look south
+        convert[(0, 1)] = (-1, 0)  # look east
+        convert[(0, -1)] = (1, 0)  # look weast
+        buff = convert[(self.x_direction, self.z_direction)]
+        self.x_direction = buff[0]
+        self.z_direction = buff[1]
+
     @classmethod
-    def origin (cls):
-        #get toon position to object
-        return cls ()
-      
+    def origin(cls):
+        """
+        get toon position to object
+        :return: Position
+        """
+        return cls()
+
     @classmethod
-    def relative_distance (cls,dX,dY,dZ):
-        #toon position + relative move
-        return cls (None,dX,dY,dZ)      
+    def relative_distance(cls, dx, dy, dz):
+        """
+        toon position + relative move
+        :param dx: float
+        :param dy: float
+        :param dz: float
+        :return: Position
+        """
+        back = cls(None, dx, dy, dz)
+        assert isinstance(back, Position)
+        return back
 
 
-if __name__ == "__main__":    #direct call for testing purpose
+if __name__ == "__main__":  # direct call for testing purpose
     # self test code
-    first = Position ()
+    first = Position()
     print first.x
     print first.x_direction
     print first.z_direction
     first.x = 2
     first.y = 3
     first.z = 4
-    second = Position ( first )
+    second = Position(first)
     print second.x
-    third = Position ( second , 2 , 3 , 4 )
+    third = Position(second, 2, 3, 4)
     print third.x
-    where_i_am = Position.origin ()
+    where_i_am = Position.origin()
     print where_i_am.x
-    where_is_house = Position.relative_distance (2,2,2)
+    where_is_house = Position.relative_distance(2, 2, 2)
     print where_is_house.x
