13a55116bc0cb9c912ca81736b1e3c05e91c3316
spavle <pavle.spoljaric@gmail.com>
2016-03-30 16:42:25 +0200

    repair

--- Position.py
+++ Position.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from mc import *  # Minecraft api import
 
 mc = Minecraft()  # initialization
@@ -22,40 +21,7 @@ class Position(object):
 
         if my_input is None:
             self.get_origin()
-=======
-from mc import * #Minecraft api import
-mc = Minecraft() #initialization
 
-
-
-class Position ( object ):
-    """
-    Class for storing and manipulating coordintes in Minecraft 
-    - x
-    - y
-    - z
-    - x_direction
-    - z_direction
-
-    __init__
-    move ?
-    copy ?
-    rotate_left
-    rotate_right
-    origin
-    """
-
-    def __init__ (self, input=None , dX=0, dY=0, dZ=0 ) :
-        """ 
-        Position initialization
-        """
-        if input != None :
-            self.x = input.x
-            self.y = input.y
-            self.z = input.z
-            self.x_direction = input.x_direction
-            self.z_direction = input.z_direction
->>>>>>> origin/master
         else:
             self.x = my_input.x
             self.y = my_input.y
@@ -63,7 +29,7 @@ class Position ( object ):
             self.x_direction = my_input.x_direction
             self.z_direction = my_input.z_direction
 
-<<<<<<< HEAD
+
         self.x = self.x + self.x_direction * dx - self.z_direction * dz  # x translate
         self.z = self.z + self.x_direction * dz + self.z_direction * dx  # y translate
         self.y += dy  # z translate
@@ -73,22 +39,7 @@ class Position ( object ):
         get current position and direction
         """
         current_position = mc.player.getPos()  # get coordinates
-=======
 
-        print self.x
-        print self.x_direction
-        print dX
-        self.x=self.x + self.x_direction*dX - self.z_direction*dZ    		# x translate
-        self.z=self.z + self.x_direction*dZ + self.z_direction*dX			# y translate
-        self.y = self.y + dY                                              # z translate
-
-   
-    def get_origin ( self ):
-        """
-        get current position and direction
-        """
-        current_position = mc.player.getPos()	#get coordinates
->>>>>>> origin/master
         self.x = current_position.x
         self.y = current_position.y
         self.z = current_position.z
@@ -98,7 +49,7 @@ class Position ( object ):
         if abs(current_direction.x) >= abs(current_direction.z):  # find dominant direction
             self.x_direction = int(round(current_direction.x))
         else:
-<<<<<<< HEAD
+
             self.z_direction = int(round(current_direction.z))
 
     def rotate_left(self):
@@ -150,55 +101,8 @@ class Position ( object ):
 if __name__ == "__main__":  # direct call for testing purpose
     # self test code
     first = Position()
-=======
-            self.z_direction=int(round(current_direction.z))  
-   
-    def rotate_left (self):
-        """
-        rotate direction to left
-        """
-        convert = {}
-        convert [ ( 1 , 0  ) ] = (  0 , -1  ) # look north
-        convert [ ( -1 , 0 ) ] = (  0 , 1 ) # look south
-        convert [ ( 0 , 1 ) ] = (  1 , 0 )  # look east
-        convert [ ( 0 , -1 ) ]= (  -1 , 0 )  # look weast
-        buff = convert [ ( self.x_direction , self.z_direction )   ]
-        self.x_direction=buff [0]
-        self.z_direction=buff [1]
-      
-    def rotate_right (self):
-        """
-        rotate direction to right
-        """
-        convert = {}
-        convert [ ( 1 , 0  ) ] = (  0 , 1  ) # look north
-        convert [ ( -1 , 0 ) ] = (  0 , -1 ) # look south
-        convert [ ( 0 , 1 ) ] = (  -1 , 0 )  # look east
-        convert [ ( 0 , -1 ) ] = (  1 , 0 )  # look weast
-        buff = convert [ ( self.x_direction , self.z_direction )   ]
-        self.x_direction=buff [0]
-        self.z_direction=buff [1]      
-   
-   
-    @classmethod
-    def origin (cls):
-        """
-        get toon position to object
-        """
-        return cls ()
-      
-    @classmethod
-    def relative_distance (cls,dX,dY,dZ):
-        """
-        toon position + relative move
-        """
-        return cls (None,dX,dY,dZ)      
 
 
-if __name__ == "__main__":    #direct call for testing purpose
-    # TDD: self test code
-    first = Position ()
->>>>>>> origin/master
     print first.x
     print first.x_direction
     print first.z_direction
--- one.py
+++ one.py
@@ -1,7 +1,7 @@
 from mc import *  # Minecraft api import
 from Position import *  # clas for position
 from cuboid import *
-<<<<<<< HEAD
+
 
 mc = Minecraft()  # initialization
 
@@ -20,34 +20,7 @@ class Block(Cuboid):
 
 if __name__ == "__main__":  # direct call for testing purpose
     # self test code
-=======
-mc = Minecraft() #initialization
 
-class Block (Cuboid):
-    """
-    class for creating one block
-    """
-    def __init__ ( self , block_position , material , material_modification = 0 ):
-        """
-        Block class initialization
-        """
-        self.draw ( block_position.x , block_position.y , block_position.z  , material , material_modification )
-      
-    def draw ( self , block_position_x , block_position_y , block_position_z  , material , material_modification ) :
-        """
-        create one  block
-        """
-        Cuboid.draw ( self , block_position_x , block_position_y , block_position_z , block_position_x , block_position_y , block_position_z , material , material_modification )
-        
-      
-      
-
-      
-      
-      
-if __name__ == "__main__":    #direct call for testing purpose
-    # TDD: self test code
->>>>>>> origin/master
 
 
     Block(Position.relative_distance(2, 1, -1), 1, 0)
